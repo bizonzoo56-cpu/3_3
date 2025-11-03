a675dd873cbb9de47edb04e81529fa5bb1a841a1
Mateusz <bizonzoo56@gmail.com>
2025-11-03 09:08:11 +0000

    3_3.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,11 +19,11 @@ public class Main {
         }
 
         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= i; j++) {
-                if (i == 1 || i == 2 || i == n || j == 1 || j == i)
-                    System.out.print(znak);
-                else
-                    System.out.print(" ");
+                System.out.print(znak);
             }
             System.out.println();
         }
